4ec015368d6c643e834dc5366cd25381172d694e
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-10-14 19:47:59 +0300

    initial books model created, admin superuser created,

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+  book_id = models.IntegerField(default=0)
+  ISBN = models.CharField(max_length=150)  
+  book_name = models.CharField(max_length=150)
+  description = models.CharField(max_length=500)
+  book_author = models.CharField(max_length=150)
+  publication_year = models.IntegerField(default=0)
+  publisher = models.CharField(max_length=150)
+  page_count = models.IntegerField(default=0)
+  
+
+
+
