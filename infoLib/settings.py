ed7e1a5b4eeddd75c554606d318c70b96a79894d
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-11-14 18:41:38 +0200

    loca_settings and production_settings divided

--- infoLib/settings.py
+++ infoLib/settings.py
@@ -10,24 +10,47 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
+# # Allow all host hosts/domain names for this site
+ALLOWED_HOSTS = ['*']
+
+DEBUG = True
+
 import os
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=0=qkr1x6(apr7whi5@z2t41m8d$is#$^i2rawbb8xgufpg(j8'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-ALLOWED_HOSTS = ['*']
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+DATABASE_URL = 'postgres://bbqmfobmaebcgy:f25dd87190929ca016c2e8d15be1675e313de0d90a7aec34762f1cb07b7d1fc5@ec2-23-23-237-68.compute-1.amazonaws.com:5432/d8p1nlobaltg1o'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -72,28 +95,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'infoLib.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_deploy',
-        'USER': 'infolib1',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
-DATABASE_URL = 'postgres://bbqmfobmaebcgy:f25dd87190929ca016c2e8d15be1675e313de0d90a7aec34762f1cb07b7d1fc5@ec2-23-23-237-68.compute-1.amazonaws.com:5432/d8p1nlobaltg1o'
-
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -126,23 +127,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-# Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/'
+# try to load local_settings.py if it exists
+try:
+    from infoLib.local_settings import *
+except Exception as e:
+    pass
