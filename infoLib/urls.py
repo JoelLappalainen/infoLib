b0ef8e362096287f459aa94574066b6bfb859540
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-11-14 19:57:50 +0200

    df

--- infoLib/urls.py
+++ infoLib/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'', include('books.urls')),
-    url(r'/', include('books.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
