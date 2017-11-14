084d93d58fc8d09faa08d15a281ec9902b9c2227
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-11-14 18:53:36 +0200

    asdf

--- infoLib/settings.py
+++ infoLib/settings.py
@@ -22,6 +22,13 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# we only need the engine name, as heroku takes care of the rest
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+    }
+}
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
