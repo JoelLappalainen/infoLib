c511b8db53d5f3e84e39b1a7a0827d9a414b83dc
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-11-15 00:26:11 +0200

    fixing

--- infoLib/wsgi.py
+++ infoLib/wsgi.py
@@ -6,8 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
 import os
+import sys
+sys.path.append("/Users/Joel/Ohjelmointi/infoLib/infoLib/settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
