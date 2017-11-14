a420a27474ad0d38eb78b542abcf8896b534932f
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-11-14 19:51:17 +0200

    add / to urls

--- infoLib/urls.py
+++ infoLib/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'', include('books.urls')),
+    url(r'/', include('books.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
