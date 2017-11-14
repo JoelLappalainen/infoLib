ba273ced3af703ff46bca43eeb38b5fff8dff513
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-11-15 00:49:36 +0200

    df o Please enter the commit message for your changes. Lines starting

--- infoLib/wsgi.py
+++ infoLib/wsgi.py
@@ -7,8 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 import os
-import sys
-sys.path.append("/Users/Joel/Ohjelmointi/infoLib/infoLib/settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
