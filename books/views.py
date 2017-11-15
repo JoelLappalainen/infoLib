29b2a60489499f96e60d0643fa73306649851294
Joel Lappalainen <joel.lappalainen@gmail.com>
2017-11-15 13:47:12 +0200

    sdf

--- books/views.py
+++ books/views.py
@@ -9,10 +9,12 @@ from datetime import datetime
 from django.utils.dateformat import DateFormat
 from django.contrib.auth.decorators import login_required
 
+
 def index(request):
     all_books = Book.objects.all()
     context = {'all_books': all_books}
     return render(request, 'books/index.html', context)
+
 
 @login_required
 def add_book(request):
@@ -29,12 +31,14 @@ def add_book(request):
             book_publisher = book_form.cleaned_data['publisher']
             book_page_count = book_form.cleaned_data['page_count']
             book_cover_URL = book_form.cleaned_data['cover_URL']
-            new_book = Book(ISBN=book_ISBN, name=book_name, description=book_description, author=book_author, publication_year=book_publication_year, publisher=book_publisher, page_count=book_page_count, cover_URL=book_cover_URL)
+            new_book = Book(ISBN=book_ISBN, name=book_name, description=book_description, author=book_author,
+                            publication_year=book_publication_year, publisher=book_publisher, page_count=book_page_count, cover_URL=book_cover_URL)
             new_book.save()
             return HttpResponseRedirect('/')
     else:
         book_form = BookForm()
     return render(request, 'books/add_book.html', {'booksurl': booksurl, 'book_form': book_form})
+
 
 def detail(request, book_id):
     try:
@@ -45,6 +49,7 @@ def detail(request, book_id):
         raise Http404("Question does not exist")
     return render(request, 'books/detail.html', {'book': book, 'all_borrowings': all_borrowings, 'all_reviews': all_reviews})
 
+
 def profile(request, user_id):
     try:
         user = User.objects.get(pk=user_id)
@@ -53,9 +58,11 @@ def profile(request, user_id):
         raise Http404("Question does not exist")
     return render(request, 'profile.html', {'user': user, 'all_borrowings': all_borrowings})
 
+
 @login_required
 def borrow(request, book_id, user_id):
-    detailurl = request.build_absolute_uri(reverse('books:detail', args=[book_id]))
+    detailurl = request.build_absolute_uri(
+        reverse('books:detail', args=[book_id]))
     book = Book.objects.get(pk=book_id)
     user = User.objects.get(pk=user_id)
     book.borrowed = True
@@ -68,9 +75,11 @@ def borrow(request, book_id, user_id):
     # return render(request, 'books/detail.html', {'detailurl': detailurl, 'book': book})
     return HttpResponseRedirect('/')
 
+
 @login_required
 def return_book(request, book_id):
-    detailurl = request.build_absolute_uri(reverse('books:detail', args=[book_id]))
+    detailurl = request.build_absolute_uri(
+        reverse('books:detail', args=[book_id]))
     book = Book.objects.get(pk=book_id)
     book.borrowed = False
     book.borrower = None
@@ -78,6 +87,7 @@ def return_book(request, book_id):
     book.save()
     # return render(request, 'books/detail.html', {'detailurl': detailurl, 'book': book})
     return HttpResponseRedirect('/')
+
 
 def register(request):
     registered = False
@@ -94,8 +104,10 @@ def register(request):
         user_form = UserForm()
     return render(request, 'register.html', {'user_form': user_form, 'registered': registered})
 
+
 def reviews(request, book_id, user_id):
-    detailurl = request.build_absolute_uri(reverse('books:detail', args=[book_id]))
+    detailurl = request.build_absolute_uri(
+        reverse('books:detail', args=[book_id]))
     if request.method == 'POST':
         review_form = ReviewForm(request.POST)
         print (review_form.errors)
@@ -105,7 +117,8 @@ def reviews(request, book_id, user_id):
             rating = review_form.cleaned_data['rating']
             book = Book.objects.get(pk=book_id)
             reviewer = request.user
-            new_review = Review(date=date, review=review, rating=rating, reviewed_book=book, reviewer=reviewer)
+            new_review = Review(
+                date=date, review=review, rating=rating, reviewed_book=book, reviewer=reviewer)
             new_review.save()
             return HttpResponseRedirect(detailurl)
     else:
